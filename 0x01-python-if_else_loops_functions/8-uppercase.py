c496e64e27a19fede2b5c54ab6c0027d19882f7d
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-03-07 22:54:44 +0100

    CHMOD fix

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
     cha = 0
     str2 = ""
     for ch in str:
-        if ord(ch) >= 97 and ord(ch) <=123:
+        if ord(ch) >= 97 and ord(ch) <= 123:
             cha = ord(ch) - 32
             str2 = str2 + chr(cha)
         else:
