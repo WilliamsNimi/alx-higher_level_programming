0ac204cf76d676acf43450a6fab6cb2a99d233ca
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-03-07 13:48:17 +0100

    Removed the literal

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\language that combines remarkable power with very clear syntax"
-str = str[39:66]
-str = str + " Python"
+str = "Python is an interpreted, interactive, object-oriented programming\langua
+ge that combines remarkable power with very clear syntax"
+str = str[39:66] + str[0:7]
 print(str)
