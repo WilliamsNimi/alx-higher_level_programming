40013ef50b507ddcba236ff4d26d836444247a1c
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-03-09 06:03:39 +0100

    correction

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -2,7 +2,7 @@
 def pow(a, b):
     power = a
     if b < 0:
-        for i in range((b*-1)+1):
+        for i in range((b*-1)+2):
             power = power / power
         return power
     for i in range(b-1):
