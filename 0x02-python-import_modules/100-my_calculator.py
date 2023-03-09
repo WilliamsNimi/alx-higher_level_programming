179e1e7e36a529be4be3624f4c161c4dcec04930
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-03-09 21:44:31 +0100

    Still calculating

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+import sys
+
+
+def calculate():
+    if len(sys.argv) == 3:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        if sys.argv[2] == "+":
+            print("{} + {} = {}".format(a, b, add(a, b)))
+        elif sys.argv[2] == "-":
+            print("{} - {} = {}".format(a, b, sub(a, b)))
+        elif sys.argv[2] == "*":
+            print("{} * {} = {}".format(a, b, mul(a, b)))
+        elif sys.argv[2] == "/":
+            print("{} / {} = {}".format(a, b, int(div(a, b))))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+    else:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+
+if __name__ == "__main__":
+    calculate()
