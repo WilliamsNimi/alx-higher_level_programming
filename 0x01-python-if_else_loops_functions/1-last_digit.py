5f68792a0a32eeaa5972a07d91a2197d4351156b
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-03-09 05:57:23 +0100

    correction

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,4 +8,4 @@ elif number % 10 == 0:
 elif number % 10 < 6:
     if (number < 0):
         number = number * -1
-    print(f"Last digit of {number:d} is {(number%10)*-1:d} and is less than 6 and not 0")
+        print(f"Last digit of {number:d} is {(number%10)*-1:d} and is less than 6 and not 0")
