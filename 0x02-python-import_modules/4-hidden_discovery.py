a870264511bb374ec2f21daabe965e5ae4747309
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-03-09 21:10:18 +0100

    hidden things like sneaky links

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from hidden_d import my_secret_santa, print_hidden, print_school
+def unhide():
+    print(my_secret_santa)
+    print(print_hidden)
+    print(print_school)
+if __name__ == "__main__":
+    unhide()
