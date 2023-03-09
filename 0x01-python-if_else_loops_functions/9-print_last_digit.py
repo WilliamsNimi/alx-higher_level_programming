078097bb98a460077fafaabe09e5f7d13046a26e
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-03-09 06:02:11 +0100

    correction

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,7 +3,7 @@ def print_last_digit(number):
     if number == 0:
         return "00"
     if number < 0:
-        mod = (number % 10) * 11
+        mod = ((number * -1) % 10) * -11
     else:
         mod = number % 10
     return mod
