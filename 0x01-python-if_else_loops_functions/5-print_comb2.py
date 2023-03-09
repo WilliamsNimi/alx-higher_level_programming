76549bb14cd65e3a2ed08147fda2fb50e86a1e23
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-03-09 05:59:06 +0100

    correction

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,7 +2,5 @@
 for i in range(0,100):
     if i < 10:
         print("0{d},".format(i), end=" ")
-    elif i >= 10 and i != 99:
+    elif i >= 10:
         print("{d},".format(i), end=" ")
-    else:
-        print("{d}".format(i))
