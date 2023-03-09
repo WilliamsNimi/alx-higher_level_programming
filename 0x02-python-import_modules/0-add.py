b758f9f582003207a273931b02a72c6aaa09868b
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-03-09 21:21:14 +0100

    Aesthetic update

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 from add_0 import add
+
+
 def addition():
     a = 1
     b = 2
     c = add(a, b)
     print("{} + {} = {}".format(a, b, c))
+
+
 if __name__ == "__main__":
     addition()
--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 from variable_load_5 import a
+
+
 def simp_var():
     print(a)
+
+
 if __name__ == "__main__":
     simp_var()
