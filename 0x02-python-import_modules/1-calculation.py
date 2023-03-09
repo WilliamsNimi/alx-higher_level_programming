a8f9273a23d9ff96bef2d255c99342672e2fdba9
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-03-09 19:21:09 +0100

    calling import only once

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-from calculator_1.py import sub
-from calculator_1.py import add
-from calculator_1.py import mul
-from calculator_1.py import div
+from calculator_1.py import add, sub, mul, div
 def calculate():
     a = 10
     b = 5
