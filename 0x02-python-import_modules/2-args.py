79ab6905a59e338ca4841c5bd6a1ff3c52f4639d
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-03-09 21:28:02 +0100

    Argument print fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,11 +5,12 @@ import sys
 def cmdargs():
     count = 1
     if len(sys.argv) == 1:
-        print("{}.".format(len(sys.argv)))
+        print("0 arguments.")
     else:
         if len(sys.argv) == 2:
-            print("{} argument:/n{}:".
-                  format(len(sys.argv), count, sys.argv[1]))
+            print("{} argument:"
+                  .format((len(sys.argv) - 1)))
+            print("{}: {}".format(count, sys.argv[1]))
         else:
             print("{} arguments:".format(len(sys.argv)-1))
             for i in range(len(sys.argv)-1):
