aff0c44e42c6ba5970f295e2c55650f012875145
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-03-12 08:39:47 +0100

    Bug fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,7 +5,7 @@ def print_matrix_integer(matrix=[[]]):
     for list_el in matrix:
         i = 0
         for nums in list_el:
-            if i < 2:
+            if i < (len(list_el) - 1):
                 print("{:d}".format(nums), end=" ")
             else:
                 print("{:d}".format(nums), end="")
