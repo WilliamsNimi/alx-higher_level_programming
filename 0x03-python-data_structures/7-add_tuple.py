61216bc946e3666bf1e01f587a02f35d00e423c5
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-03-12 08:44:46 +0100

    Bug fix

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -20,4 +20,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
         return tuple_b
     if len(tuple_b) == 0 and len(tuple_a) != 0:
         return tuple_a
-    return ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
+    if len(tuple_a) >= 2 and len(tuple_b) >= 2:
+        return ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
+    return ()
