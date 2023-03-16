3c08cf4a73c8c30cf5b6817444c5240fdc4af517
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-03-16 21:08:41 +0100

    complex delete

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for key, values in a_dictionary.items():
-        if values == value:
-            del a_dictionary(key)
+    values_list = list(a_dictionary.values())
+    keys_list = list(a_dictionary.keys())
+    if value in values_list:
+        a_dictionary.pop(keys_list[values_list.index(value)])
     return a_dictionary
