6c229ef5361a3e8d9f8f9afca13676574f4efde1
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-03-16 17:47:56 +0100

    The Romans lost

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+def roman_to_int(roman_string):
+    new_dict = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500}
+    sum_ = 0
+    if roman_string is None:
+        return 0
+    for i in range(0, len(roman_string)):
+        if i > 0:
+            if roman_string[i] == 'V' or roman_string[i] == 'X':
+                if roman_string[i - 1] == 'I':
+                    sum_ = sum_ + new_dict[roman_string[i]] - 2
+                else:
+                    sum_ = sum_ + new_dict[roman_string[i]]
+            else:
+                sum_ = sum_ + new_dict[roman_string[i]]
+        else:
+            sum_ = sum_ + new_dict[roman_string[i]]
+    return sum_
