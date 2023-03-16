b0f8179d032091ba70db376b67cac2a29ccde687
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-03-16 21:10:57 +0100

    pycodestyle checks

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 
 
 def roman_to_int(roman_string):
-    new_dict = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500}
+    new_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500}
     sum_ = 0
     if roman_string is None:
         return 0
