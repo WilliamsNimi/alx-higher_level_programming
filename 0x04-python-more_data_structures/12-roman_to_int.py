7d9572725d9e3f9d76eb08d4046be79c66a3a2f5
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-03-16 21:15:44 +0100

    Refix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,7 @@
 def roman_to_int(roman_string):
     new_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500}
     sum_ = 0
-    if roman_string is None or !isinstance(roman_string, str):
+    if roman_string is None or type(roman_string) != str:
         return 0
     for i in range(0, len(roman_string)):
         if i > 0:
