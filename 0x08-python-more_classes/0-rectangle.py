a859394ea663c680b203cb620991951cdd231db8
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-04-03 22:42:00 +0100

    added Docs

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
+# -*- coding: utf-8 -*-
 """ This module contains the rectangle class """
 class Rectangle:
+    """ This is the rectangle class """
+
     def __init__(self):
         """ This is the __init__ method
            Args:
