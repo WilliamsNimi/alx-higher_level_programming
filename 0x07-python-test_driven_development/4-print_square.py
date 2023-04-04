24ea69be65cdff31be0e62e3fbc4fe2c142de4ab
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-04-04 16:29:31 +0100

    Created test documents and python programs for projects 0 to 4

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def print_square(size):
+    """ This function prints a square with # """
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    if type(size) == float and size < 0:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(0, size):
+        for i in range(0, size):
+            print("#", end="")
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/4-print_square.txt", optionflags=ELLIPSIS)
