85f1c9f78edb38c83faa72896790cac7124d39b0
Nimi Williams <williamsnimi@Williams_Nimi_M.localdomain>
2023-05-18 16:03:14 +0100

    Created scripts 10 to 15

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+--List all records of a table
+SELECT (score, name)
+FROM second_table
+ORDER BY score;
---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,4 @@
+--Number by score
+SELECT * FROM second_table
+GROUP BY score
+SORT BY score;
