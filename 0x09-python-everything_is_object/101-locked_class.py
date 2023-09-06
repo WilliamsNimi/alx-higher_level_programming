5258db6f896a0795c6f3c5aa9c21a8a9532fa163
WilliamsNimi <williamson.nimi@gmail.com>
2023-09-06 22:32:31 +0100

    Cosmetic Update
    Updated the locked class documentation

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """
-This is the LockedClass
+This is the LockedClass Module
 """
 class LockedClass:
+    """
+    This is the LockedClass Class
+    """
     def __setattr__(self, __name, __value):
         """This is the dunder methos __setattr__"""
         if __name == "first_name":
