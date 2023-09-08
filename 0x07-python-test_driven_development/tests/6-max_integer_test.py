e02e75860d12ee212671a49bcb5bc0f03254a5be
WilliamsNimi <williamson.nimi@gmail.com>
2023-09-08 21:22:50 +0100

    Deleted unutilized test cases

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -9,12 +9,6 @@ class TestMaxInteger(unittest.TestCase):
     def tests_max_integer(self):
         """ This is a test function to test max_integer function """
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
-        self.assertEqual(max_integer([4, 2, 3, 1]), 4)
-        self.assertEqual(max_integer([1, 2, 5, 3, 4]), 5)
-        self.assertEqual(max_integer([-1, 2, 5, 3, 4]), 5)
-        self.assertEqual(max_integer([-1, -2, -5, -3, -4]), 5)
-        self.assertEqual(max_integer([5]), 5)
-
 
 if __name__ == '__main__':
     unittest.main()
