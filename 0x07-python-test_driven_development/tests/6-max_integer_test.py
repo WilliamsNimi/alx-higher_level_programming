0d910da7b8ce47fcb132fb76b10b56ed7d59b2b0
WilliamsNimi <williamson.nimi@gmail.com>
2023-09-08 21:34:35 +0100

    Test Cases Updated
    Updated the Test cases to include edge cases

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -10,6 +10,11 @@ class TestMaxInteger(unittest.TestCase):
         """ This is a test function to test max_integer function """
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
         self.assertEqual(max_integer([4, 2, 3, 1]), 4)
+        self.assertEqual(max_integer([1, 2, 5, 3, 4]), 5)
+        self.assertEqual(max_integer([-1. 2. 3. 4]), 4)
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer([5]), 5)
 
 if __name__ == '__main__':
     unittest.main()
