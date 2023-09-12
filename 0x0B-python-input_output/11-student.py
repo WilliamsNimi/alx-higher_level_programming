494b2c288b0f254ec65c0fa52d7682e718ea7d81
WilliamsNimi <williamson.nimi@gmail.com>
2023-09-12 21:36:25 +0100

    11-Student
    Uploading the 11-Student file for serialization Deserialization

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" This is a Student class module """
+
+
+class Student:
+    """ This is the Student Class """
+    first_name = ""
+    last_name = ""
+    age = None
+
+    def __init__(self, first_name, last_name, age):
+        """ This is the __init__ function """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ This function converts dictionary rep of the class to Json """
+        obj_dict1 = self.__dict__
+        obj_dict = {}
+        if attrs != None:
+            for items in attrs:
+                if items in obj_dict1:
+                    obj_dict[items] = obj_dict1[items]
+            return (obj_dict)
+        else:
+            return (self.__dict__)
+
+    def reload_from_json(self, json):
+        """This function reads and converts json to dictionary"""
+        self.__dict__ = json
