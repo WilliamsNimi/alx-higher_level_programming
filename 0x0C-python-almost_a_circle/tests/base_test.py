c86c435ca2403d4a9b8c057804e25cf7dabe3002
WilliamsNimi <williamson.nimi@gmail.com>
2023-09-22 21:52:59 +0100

    Added Test Cases
    Added a few Test Cases

--- 0x0C-python-almost_a_circle/tests/base_test.py
+++ 0x0C-python-almost_a_circle/tests/base_test.py
@@ -6,13 +6,22 @@ base = __import__('models/base').base
 class TestBase(unittest.TestCase):
     """ This is the base Test class """
 
-    def tests_to_json_string(self):
+    def test_to_json_string(self):
         """ This is the test function for to_json_string function"""
-        aelf.assertEqual(type(to_json_string(self.__dict__)), dict)
+        self.assertEqual(type(base.to_json_string(self.__dict__)), dict)
+        self.assertEqual(type(base.to_json_string([]), list))
+        self.assertEqual(base.to_json_string([ { 'id': 12 }]), [{"id": 12}])
+    
+    def test_from_json_string(self):
+        """This is the test function for the from_json_string"""
+        self.assertEqual(type(base.from_json_string(None)), list)
+        self.assertEqual(type(base.from_json_string("[]")), list)
+        self.assertEqual(base.from_json_string('[{ "id": 89 }]'), [{"id":89}])
 
-    def tests_save_to_file(self):
-        """ This is the test function for save_to_file function """
-        pass
+    def test___init__(self):
+        """This function tests the Base constructor"""
+        self.assertEqual(base.id, None)
+        self.assertEqual(base.__nb_objects, 1)
 
 if __name__ == '__main__':
     unittest.main()
