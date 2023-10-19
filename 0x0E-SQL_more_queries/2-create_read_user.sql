060fb5fb28dd83e53b74eaf6f9f8fb941b76df38
WilliamsNimi <williamson.nimi@gmail.com>
2023-10-19 22:49:00 +0100

    Bug FIX
    Added Grant Usage

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- CREATE DATABASE AND READ ONLY USER
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER `user_0d_2`@`localhost` IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON `hbtn_0d_2` TO `user_0d_2`@`localhost`;
+GRANT USAGE ON *.* TO `user_0d_2`@`localhost`;
 GRANT SELECT ON `hbtn_0d_2`.* TO `user_0d_2`@`localhost`
