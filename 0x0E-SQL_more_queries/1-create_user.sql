65667fc2571db1830adb47c56ec5d9ecc3899c9f
WilliamsNimi <williamson.nimi@gmail.com>
2023-11-05 20:26:37 +0100

    Bug Fix
    Added DB

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,2 @@
 -- create a user and grant privileges
-GRANT ALL PRIVILEGES ON * TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
