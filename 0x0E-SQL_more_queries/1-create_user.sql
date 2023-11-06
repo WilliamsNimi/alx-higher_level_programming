4171fa5b5656550bedb3af19f9604542cd93e16c
WilliamsNimi <williamson.nimi@gmail.com>
2023-11-06 09:30:05 +0100

    Update
    Tried splitting the statements into two

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
 -- create a user and grant privileges
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+UPDATE mysql.user SET Password=PASSWORD('user_0d_1_pwd') WHERE USER='user_0d_1';
