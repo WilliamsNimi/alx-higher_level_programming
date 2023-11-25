1d1eae7c79758534e212e9636a424650e240649e
root <williamson.nimi@gmail.com>
2023-11-25 02:31:13 -0800

    Fixed pycodestyle issues

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,15 +3,17 @@
 import MySQLdb
 import sys
 
-def database_access(user, pword, database):
+
+def database_access(u, pw, db):
     """
     Accessing the holberton DB via this function
     """
-db = MySQLdb.connect(user=user, passwd=pword, db=database, host="localhost", port=3306)
-cur = db.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-for row in cur.fetchall():
-    print(row)
+    db = MySQLdb.connect(user=u, passwd=pw, db=db, host="localhost", port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    for row in cur.fetchall():
+        print(row)
+
 
 if __name__ == "__main__":
     database_access(sys.argv[1], sys.argv[2], sys.argv[3])
