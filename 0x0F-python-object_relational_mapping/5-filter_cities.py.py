c0547cd5bbfa046abcf9d7f8d6f97edce2614b93
WilliamsNimi <williamson.nimi@gmail.com>
2023-12-26 21:42:46 +0100

    Error fix

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Python module for MYSQLdb Library """
+import MySQLdb
+import sys
+
+
+def database_access(u, pw, db, s):
+    """
+    Accessing the holberton DB via this function
+    """
+    db = MySQLdb.connect(user=u, passwd=pw, db=db, host="localhost", port=3306)
+    c = db.cursor()
+    c.execute("SELECT c.name FROM cities c \
+JOIN states s ON c.state_id=s.id WHERE s.name=%s ORDER BY c.id ASC", (s))
+    rows = c.fetchall()
+    results = ''
+    for i in range(len(rows)):
+        if i == len(rows) - 1:
+            results += rows[i][0]
+        else:
+            results += rows[i][0] + ', '
+    print(results)
+
+
+if __name__ == "__main__":
+    database_access(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
